643d87973dd6008e32b7d0dcc55d717aa5a572c0
ulzii5314-ui <ulzii5314@gmail.com>
2026-08-05 01:05:57 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// Хөгжим Өлзий Систем — Тохиргоо
-// Багш: Л.Өлзийцэцэг
-// Үүсгэсэн: 8/4/2026, 9:51:16 PM
-window.SUPABASE_URL = "https://qwmcqmqefowbiarlvvjv.supabase.co";
-window.SUPABASE_KEY = "sb_publishable_ycNFhVOcjwCpTF9Zx4I7Jw_n0g6J7IQ";
-window.TEACHER_NAME = "Л.Өлзийцэцэг";
